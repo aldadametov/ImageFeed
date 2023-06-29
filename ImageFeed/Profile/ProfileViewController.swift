c6c1dc0e9fcdf9bd807bd450018dbda0fe79aa8b
Alisher Dadametov <ksindao@gmail.com>
2023-06-29 13:46:22 +0300

    - свёрстан экран профиля пользователя

--- ImageFeed/Profile/ProfileViewController.swift
+++ ImageFeed/Profile/ProfileViewController.swift
@@ -9,4 +9,11 @@ import UIKit
 
 final class ProfileViewController: UIViewController {
     
+    @IBOutlet var avatarImageView: UIImageView!
+    @IBOutlet var nameLabel: UILabel!
+    @IBOutlet var loginNameLabel: UILabel!
+    @IBOutlet var descriptionLabel: UILabel!
+    @IBOutlet var logoutButton: UIButton!
+    @IBAction func didTapLogoutButton() {
+    }
 }

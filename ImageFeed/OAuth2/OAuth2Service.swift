8fccb12ad1913e07956c647c9fbe3a57cbce998c
Alisher Dadametov <ksindao@gmail.com>
2023-07-31 13:14:06 +0300

    - Добавлена блокировка UI при авторизации для устранения гонки.

--- ImageFeed/OAuth2/OAuth2Service.swift
+++ ImageFeed/OAuth2/OAuth2Service.swift
@@ -24,7 +24,8 @@ final class OAuth2Service {
     
     static let shared = OAuth2Service()
     private let urlSession = URLSession.shared
-    
+    private var task: URLSessionTask?
+    private var lastCode: String?
     private (set) var authToken: String? {
         get {
             return OAuth2TokenStorage().token
@@ -37,19 +38,28 @@ final class OAuth2Service {
     func fetchOAuthToken(
         _ code: String,
         completion: @escaping (Result<String, Error>) -> Void ){
+            assert(Thread.isMainThread)
+            if lastCode == code {return}
+            task?.cancel()
+            lastCode = code
             let request = authTokenRequest(code: code)
             let task = object(for: request) { [weak self] result in
-                guard let self = self else { return }
-                switch result {
-                case .success(let body):
-                    let authToken = body.accessToken
-                    self.authToken = authToken
-                    completion(.success(authToken))
-                case .failure(let error):
-                    completion(.failure(error))
-                } }
+                DispatchQueue.main.async {
+                    guard let self = self else { return }
+                    switch result {
+                    case .success(let body):
+                        let authToken = body.accessToken
+                        self.authToken = authToken
+                        completion(.success(authToken))
+                        self.task = nil
+                    case .failure(let error):
+                        completion(.failure(error))
+                        self.lastCode = nil
+                    }
+                }
+            }
+            self.task = task
             task.resume()
-            
         }
 }
 
